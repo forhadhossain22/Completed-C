0909d1586999c97630ad675e8fb7657da402319a
Forhad Hossain <104172772+forhadhossain22@users.noreply.github.com>
2022-11-29 19:28:56 +0600

    Add files via upload

---
+++ Operator and Expression/exp1.c
@@ -0,0 +1,33 @@
+// Examples of arithmetic operators in C
+
+#include <stdio.h>
+
+int main()
+
+{
+
+    int a = 7,b = 5, c;
+
+    c = a+b;
+
+    printf("a+b = %d \n",c);
+
+    c = a-b;
+
+    printf("a-b = %d \n",c);
+
+    c = a*b;
+
+    printf("a*b = %d \n",c);
+
+    c = a/b;
+
+    printf("a/b = %d \n",c);
+
+    c = a%b;
+
+    printf("Remainder when a is divided by b = %d \n",c);
+
+    return 0;
+
+}
