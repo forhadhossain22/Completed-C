a55900a0d853671976bad92c9abe88ad8c61e43f
Forhad Hossain <104172772+forhadhossain22@users.noreply.github.com>
2022-12-11 17:52:48 +0600

    Add files via upload

---
+++ Control Statement/Nested Loop - Pattern/exp7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+
+    int n,row,col;
+    printf("Enter your number: ");
+    scanf("%d",&n);
+
+    for(row=1; row<=n; row++){
+        for(col=1; col<=row; col++){
+            printf("%c ",col+64);
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
---
+++ Control Statement/Nested Loop - Pattern/exp8.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+
+    int n,row,col;
+    printf("Enter your number: ");
+    scanf("%d",&n);
+
+    for(row=1; row<=n; row++){
+        for(col=1; col<=row; col++){
+            printf("%c ",col+96);
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
+
